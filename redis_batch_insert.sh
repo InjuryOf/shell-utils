7936b42f7c27b703faecc98fa1479cc600a2358e
杨磊 <lei.yang@xianghuanji.com>
2021-04-09 14:28:01 +0800

    fix

--- redis_batch_insert.sh
+++ redis_batch_insert.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 # desc : redis batch insert
 
-for((i=1;i<100;i++))
+# 批量生成hash插入语句
+for((i=1;i<=1000;i++))
 do
-    echo "set k1101$i v3301$i" >> data/redis_batch_insert.txt
+   for((j = 1; j<=1000;j++))
+   do
+        echo "hset 1001$i $j 3301$i$j" >> data/redis_batch_insert.txt
+   done
 done
+
+# 批量生成string插入语句
+# for((i=1;i<=1000000;i++))
+# do
+#     echo "set 1101$i 3301$i" >> data/redis_batch_insert.txt
+# done
